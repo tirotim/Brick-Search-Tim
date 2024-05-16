890bdf7e4a974e5cf595cef524fe7458c67510ad
Tim <tim@tiromedia.co.uk>
2024-05-16 16:12:57 +0100

    Initial Commit

---
+++ Brick Search Tim/Brick_Search_TimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brick_Search_TimApp.swift
+//  Brick Search Tim
+//
+//  Created by Timothy Doel on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Brick_Search_TimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brick Search Tim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brick Search Tim
+//
+//  Created by Timothy Doel on 16/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
